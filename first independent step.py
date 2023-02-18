9612b103721dd6e367175ad0a92775d846b2b207
Abdurahmon <124136395+MrAbdurahmon@users.noreply.github.com>
2023-02-18 04:47:13 +0300

    Add files via upload

---
+++ first independent step.py
@@ -0,0 +1,17 @@
+# Task.1
+print('Assalomu alaykum')
+name = input('Ismingizni kiriting: ')
+lastname = input('Familiyangizni kiriting: ')
+age = input('Yoshingizni kiriting: ')
+print('Assalomu alaykum ' + '' + name+ ' ' + lastname)
+
+
+# #Task.2
+# z = input("oquvchilar soni: ")
+# k = input('olmalr soni: ')
+# print("savatta qoldi " + str(int(k) % int(z)))
+# print("har bir o`quvchiga " + str(int(k) // int(z)) + ' dan olma tegdi .')
+#
+# a = input('Raqamingizni kiriting: ')
+# print(a + "dan oldingi raqam bu: " + str(int(a)-1) )
+# print(a + 'dan keyingi raqam bu: ' + str(int(a)+1) )
